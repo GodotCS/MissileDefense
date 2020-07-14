8ac296edfdfaf341325f05d3a3ff21336e8fbc81
Timothy Ruswick <tim@gamedevunderground.com>
2020-07-14 02:30:38 -0400

    4_MD_GCS - Using The Input Map

--- bullet.cs
+++ bullet.cs
@@ -9,7 +9,6 @@ public class bullet : Area2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        GD.Print("HELLO WORLD!");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
