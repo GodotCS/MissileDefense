9c9c8ed27d64831fa8e0bb2cf807a1f1a41c41cd
Timothy Ruswick <tim@gamedevunderground.com>
2020-07-14 02:39:51 -0400

    15_MD_GCS - Game Over & Restarting Game

--- scripts/player.cs
+++ scripts/player.cs
@@ -5,6 +5,7 @@ public class player : Node
 {
     bulletBrain bulletBrain;
     public bool canShoot = true;
+    public bool gameOver = false;
     public int health = 3;
     public int score = 0;
 
@@ -13,6 +14,14 @@ public class player : Node
     {
         bulletBrain = (bulletBrain)GetNode("/root/game/bullets/bulletBrain");
         updateUI();
+    }
+
+    public override void _Input(InputEvent _inputEvent)
+    {
+        if((_inputEvent.IsActionPressed("click")) && (gameOver == true))
+        {
+            GetTree().ReloadCurrentScene();
+        }
     }
 
     public void _on_playerHitZone_area_entered(Area2D bullet)
@@ -30,6 +39,20 @@ public class player : Node
     {
         health = Math.Max(health-damageAmount,0);
         updateUI();
+
+        if((health <= 0) && (gameOver != true))
+        {
+            gameOver = true;
+            canShoot = false;
+
+            var gameOverScreen = (Node2D)GetNode("/root/game/hud/gameOverScreen");
+            gameOverScreen.Visible = true;
+
+            var cannon = (Node2D)GetNode("/root/game/foreground/cannon");
+            bulletBrain.spawnExplosion(cannon.GlobalPosition,"enemy");
+            cannon.QueueFree();
+
+        }
     }
 
     public void addScore(int scoreAmount = 1)
