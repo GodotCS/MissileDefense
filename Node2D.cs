d54626e31deccfd1d3512995b9f16a5fe4891f49
Timothy Ruswick <tim@gamedevunderground.com>
2020-07-14 02:15:21 -0400

    4_IN_GCS

---
+++ Node2D.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public class Node2D : Godot.Node2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GD.Print("HELLO WORLD!");
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
